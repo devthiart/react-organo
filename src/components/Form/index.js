b82537e9d094b009b93350648ca285db6e48cb4e
devthiart <devthiart@gmail.com>
2023-12-18 16:46:50 -0300

    Class 02

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,34 @@
 import './Form.css';
 import TextField from '../TextField';
+import DropdownList from '../DropdownList';
+import Button from '../Button';
+
+const submitForm = (event) => {
+  event.preventDefault();
+  console.log(event);
+  console.log("Saving data...");
+}
 
 const Form = () => {
+  const team = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
+
   return (
     <section className="container-form">
-      <form>
-        <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <TextField label="Nome" placeholder="Digite seu nome" />
-        <TextField label="Cargo" placeholder="Digite seu cargo" />
+      <form onSubmit={submitForm}>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <TextField label="Nome" placeholder="Digite seu nome" isRequired={true}/>
+        <TextField label="Cargo" placeholder="Digite seu cargo" isRequired={true} />
         <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
+        <DropdownList label="Time" items={team} isRequired={true} />
+        <Button>Criar Card</Button>
       </form>
     </section>
   )
--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -6,7 +6,7 @@ const TextField = (props) => {
   return (
     <div className='text-field'>
       <label>{props.label}</label>
-      <input placeholder={modifiedPlaceholder} />
+      <input placeholder={modifiedPlaceholder} required={props.isRequired} />
     </div>
   )
 }
